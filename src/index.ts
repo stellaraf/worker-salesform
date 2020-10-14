7a89c179b1489fb5f973ce3e6c78a978b70b2282
checktheroads <matt@stellar.tech>
2020-10-14 10:12:49 -0700

    add CORS headers

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import { buildUserData } from './userData';
 const RES_HEADERS = {
   'Content-Type': 'application/json',
   'Access-Control-Allow-Headers': 'Content-Type, x-stellar-site-form-sales-key, User-Agent',
+  'Access-Control-Allow-Origin': 'https://stellar.tech,https://stellar.af,http://localhost:3000',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
 };
 
 /**
